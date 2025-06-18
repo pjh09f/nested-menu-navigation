1a33ea68a7de31a45f714d42bf697bf1a238d15b
pjh09f <pjh09f@fsu.edu>
2025-06-19 01:28:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+//toggles visibility of a specific subject's topics
 function toggleTopics(subject) {
+    //gets the topic section related to the clicked subject
     const topics = document.getElementById(`${subject}-topics`);
+  
+    //hides all other topic sections
     const allTopics = document.querySelectorAll('.topics');
     allTopics.forEach(function(div) {
       if (div !== topics) {
         div.style.display = 'none';
       }
     });
+  
+    //toggle the clicked section between visible and hidden
     if (topics.style.display === 'block') {
       topics.style.display = 'none';
     } else {
